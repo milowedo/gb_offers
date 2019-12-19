fce31d034bf918175024aa2a727428bbc9dcb0dd
milowedo <miloszszwedo@gmail.com>
2019-12-19 23:49:18 +0100

    basic api call to prove it works, using Gson

--- src/main/java/com/getbooks/gb_offers/controllers/OffersController.java
+++ src/main/java/com/getbooks/gb_offers/controllers/OffersController.java
@@ -1,17 +1,34 @@
 package com.getbooks.gb_offers.controllers;
 
 import com.getbooks.gb_offers.models.BookRequestBody;
-import com.getbooks.gb_offers.models.CalculatedOffersResponseBody;
+import com.getbooks.gb_offers.models.ApiCallResult;
+import com.getbooks.gb_offers.tasks.AllegroRequestTask;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
 
 @RestController
 public class OffersController {
 
     @PostMapping("/calculate")
-    public CalculatedOffersResponseBody receiveLoadOfBooks(@RequestBody BookRequestBody requestBody) {
-        CalculatedOffersResponseBody calculated = new CalculatedOffersResponseBody();
+    public ArrayList<ApiCallResult> receiveLoadOfBooks(@RequestBody BookRequestBody requestBody) {
+
+        AllegroRequestTask.authorizationString = requestBody.authorization;
+
+        var singleResponse = AllegroRequestTask.callApi(requestBody.books.get(0));
+        var parser = new JsonParser();
+        singleResponse.thenAccept(body -> {
+            parser.parse(body)
+                    .getAsJsonObject()
+                    .get("items")
+                    .getAsJsonObject().get("regular");
+
+        });
+
+        var calculated = new ArrayList<ApiCallResult>();
         return calculated;
     }
 }
