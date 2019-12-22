e436aae80ddcb9548e7b4a2d49eeb3cabcbaca92
milowedo <miloszszwedo@gmail.com>
2019-12-22 23:03:55 +0100

    calculations are now made for all the books in the request async, and after completion of all tasks, the result is returned as Map, where sellersId is key and a list of books is value

--- src/main/java/com/getbooks/gb_offers/controllers/OffersController.java
+++ src/main/java/com/getbooks/gb_offers/controllers/OffersController.java
@@ -1,34 +1,47 @@
 package com.getbooks.gb_offers.controllers;
 
-import com.getbooks.gb_offers.models.BookRequestBody;
-import com.getbooks.gb_offers.models.ApiCallResult;
-import com.getbooks.gb_offers.tasks.AllegroRequestTask;
+import com.getbooks.gb_offers.models.OffersEndpointRequestBody;
+import com.getbooks.gb_offers.models.BookResult;
+import com.getbooks.gb_offers.models.Seller;
+import com.getbooks.gb_offers.tasks.AllegroRequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.google.gson.JsonParser;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class OffersController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
     @PostMapping("/calculate")
-    public ArrayList<ApiCallResult> receiveLoadOfBooks(@RequestBody BookRequestBody requestBody) {
+    public ResponseEntity<Object> getCalculatedOffers(@RequestBody OffersEndpointRequestBody requestBody) {
+        var receivedAth = requestBody.authorization;
+        var receivedBooks = requestBody.books;
 
-        AllegroRequestTask.authorizationString = requestBody.authorization;
+        if (receivedBooks == null || receivedAth == null) {
+            logger.error("Request body had wrong structure");
+            return ResponseEntity.status(400).body("Request body should contain fields: authorization and books");
+        }
 
-        var singleResponse = AllegroRequestTask.callApi(requestBody.books.get(0));
-        var parser = new JsonParser();
-        singleResponse.thenAccept(body -> {
-            parser.parse(body)
-                    .getAsJsonObject()
-                    .get("items")
-                    .getAsJsonObject().get("regular");
+        AllegroRequestHandler.authorizationString = receivedAth;
+        var calculatedResult = new ConcurrentHashMap<Seller, HashSet<BookResult>>();
+        try {
+            CompletableFuture
+                    .allOf(receivedBooks.parallelStream()
+                            .map(book -> AllegroRequestHandler.addOffersForBook(book, calculatedResult))
+                            .toArray(CompletableFuture[]::new)).get();
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error("Something went wrong while analyzing tasks" + e.getMessage());
+        }
 
-        });
-
-        var calculated = new ArrayList<ApiCallResult>();
-        return calculated;
+        return ResponseEntity.ok().body(calculatedResult);
     }
 }
